c517526ad956dc013cbc59cb63d16c88b4683fe5
Shrutika Rahatekar <shrutikarahatekar43@gmail.com>
2020-05-01 01:31:34 +0530

    parcially done

---
+++ app/src/main/java/com/example/recyclerviewdemo/MyListData1.java
@@ -0,0 +1,22 @@
+package com.example.recyclerviewdemo;
+
+public class MyListData1{
+    private String description;
+    private int imgId;
+    public MyListData1(String description, int imgId) {
+        this.description = description;
+        this.imgId = imgId;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public int getImgId() {
+        return imgId;
+    }
+    public void setImgId(int imgId) {
+        this.imgId = imgId;
+    }
+}
---
+++ app/src/main/java/com/example/recyclerviewdemo/second.java
@@ -0,0 +1,31 @@
+package com.example.recyclerviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class second extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        MyListData[] myListData = new MyListData[] {
+                new MyListData("Email", android.R.drawable.ic_dialog_email),
+                new MyListData("Info", android.R.drawable.ic_dialog_info),
+                new MyListData("Delete", android.R.drawable.ic_delete),
+                new MyListData("Dialer", android.R.drawable.ic_dialog_dialer),
+                new MyListData("Alert", android.R.drawable.ic_dialog_alert),
+
+        };
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView1);
+        MyListAdapter adapter = new MyListAdapter(myListData);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+}
